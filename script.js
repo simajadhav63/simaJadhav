42fb703a57bfb33dade07fec2ccab56b2950099e
simajadhav63 <143277453+simajadhav63@users.noreply.github.com>
2024-02-28 15:57:32 +0530

    Add files via upload
    Done with this modify.

---
+++ script.js
@@ -0,0 +1,11 @@
+let div1 = document.getElementById("div1");
+
+  div1.style.backgroundColor = "yellow"; 
+  div1.style.color = "blue"; 
+  div1.style.margin = "20px";
+  div1.style.padding = "10px"; 
+  div1.style.fontSize = "18px"; 
+  div1.style.fontWeight = "bold"; 
+  div1.style.height = "200px"; 
+  div1.style.width = "300px"; 
+ 
